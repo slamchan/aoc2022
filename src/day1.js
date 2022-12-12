86449e5a69c5a4b8c21b00e3b3f85c177ff08526
Leo Qvarnström <leo.w.qvarnstrom@gmail.com>
2022-12-12 20:57:30 +0200

    Day 1 part  Please enter the commit message for your changes. Lines starting

--- src/day1.js
+++ src/day1.js
@@ -1,9 +1,25 @@
 const { readInput } = require('./utils');
 
 console.log('AoC 2022 - Day 1 part 1');
-const input = Math.max(
-  ...readInput('day1-1')
-    .split('\r\n\r\n')
-    .map((i) => i.split('\r\n').reduce((a, b) => a + Number(b), 0))
+console.log(
+  Math.max(
+    ...readInput('day1-1')
+      .split('\r\n\r\n')
+      .map((i) => i.split('\r\n').reduce((a, b) => a + Number(b), 0))
+  )
 );
-console.log(input);
+
+console.log('\r\nAoC 2022 - Day 1 part 2');
+const caloriesByElf = readInput('day1-1')
+  .split('\r\n\r\n')
+  .map((i) => i.split('\r\n').reduce((a, b) => a + Number(b), 0));
+const maxValues = [];
+while (maxValues.length < 3) {
+  maxValues.push(
+    ...caloriesByElf.splice(
+      caloriesByElf.indexOf(Math.max(...caloriesByElf)),
+      1
+    )
+  );
+}
+console.log(maxValues.reduce((a, b) => a + b, 0));
