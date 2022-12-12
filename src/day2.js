6212323e40dbed40fc6d4f730a7d160a2a4213e4
Leo Qvarnström <leo.w.qvarnstrom@gmail.com>
2022-12-12 21:52:32 +0200

    Removed extra console loggings

--- src/day2.js
+++ src/day2.js
@@ -10,7 +10,6 @@ const shapes = { X: 1, Y: 2, Z: 3, A: 1, B: 2, C: 3 };
 const results = { w: 6, d: 3, l: 0 };
 
 const calRes = (a, b) => {
-  console.log(shapes[a] + '-' + shapes[b]);
   if (shapes[b] !== 1 && shapes[b] === shapes[a] + 1)
     return shapes[b] + results.w;
   if (shapes[b] === 1 && shapes[a] === 3) return shapes[b] + results.w;
@@ -22,8 +21,9 @@ console.log(
   parsed
     .map((p) => {
       const res = calRes(p[0], p[1]);
-      console.log(`${p[0]} - ${p[1]} => ${res}`);
       return res;
     })
     .reduce((a, b) => a + b, 0)
 );
+
+console.log('\r\nAoC 2022 - Day 2 part 2');
