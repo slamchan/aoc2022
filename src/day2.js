7dd02ce2b4a7852365add9ee2021394b49f1d39a
Leo Qvarnström <leo.w.qvarnstrom@gmail.com>
2022-12-12 21:51:37 +0200

    Day 2 Part 1

---
+++ src/day2.js
@@ -0,0 +1,29 @@
+const { readInput } = require('./utils');
+
+console.log('AoC 2022 - Day 2 part 1');
+
+const parsed = readInput('day2-1')
+  .split('\r\n')
+  .map((r) => r.split(' '));
+
+const shapes = { X: 1, Y: 2, Z: 3, A: 1, B: 2, C: 3 };
+const results = { w: 6, d: 3, l: 0 };
+
+const calRes = (a, b) => {
+  console.log(shapes[a] + '-' + shapes[b]);
+  if (shapes[b] !== 1 && shapes[b] === shapes[a] + 1)
+    return shapes[b] + results.w;
+  if (shapes[b] === 1 && shapes[a] === 3) return shapes[b] + results.w;
+  if (shapes[b] === shapes[a]) return shapes[b] + results.d;
+  return shapes[b] + results.l;
+};
+
+console.log(
+  parsed
+    .map((p) => {
+      const res = calRes(p[0], p[1]);
+      console.log(`${p[0]} - ${p[1]} => ${res}`);
+      return res;
+    })
+    .reduce((a, b) => a + b, 0)
+);
