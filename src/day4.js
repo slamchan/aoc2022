cd7210ae5841eb577d480f17dd6965d39c46a86a
Leo <leo.qvarnstrom@edu.savonia.fi>
2022-12-24 11:28:49 +0200

    Day 4 part 2

--- src/day4.js
+++ src/day4.js
@@ -6,12 +6,48 @@ const parsed = readInput('day4-1').split('\r\n');
 
 let res = 0;
 parsed.forEach((x) => {
-  const pairs = x.split(',').map((p) => p.split('-').map(n => Number(n)));
-  if (pairs[0][0] >= pairs[1][0] && pairs[0][0] <= pairs[1][1] && pairs[0][1] <= pairs[1][1] && pairs[0][1] >= pairs[1][0]) {
+  const pairs = x.split(',').map((p) => p.split('-').map((n) => Number(n)));
+  if (
+    pairs[0][0] >= pairs[1][0] &&
+    pairs[0][0] <= pairs[1][1] &&
+    pairs[0][1] <= pairs[1][1] &&
+    pairs[0][1] >= pairs[1][0]
+  ) {
     res += 1;
     return;
   }
-  if (pairs[1][0] >= pairs[0][0] && pairs[1][0] <= pairs[0][1] && pairs[1][1] <= pairs[0][1] && pairs[1][1] >= pairs[0][0]) {
+  if (
+    pairs[1][0] >= pairs[0][0] &&
+    pairs[1][0] <= pairs[0][1] &&
+    pairs[1][1] <= pairs[0][1] &&
+    pairs[1][1] >= pairs[0][0]
+  ) {
+    res += 1;
+    return;
+  }
+  return;
+});
+
+console.log(res);
+
+console.log('AoC 2022 - Day 4 part 2');
+
+res = 0;
+parsed.forEach((x) => {
+  const pairs = x.split(',').map((p) => p.split('-').map((n) => Number(n)));
+  if (pairs[0][0] >= pairs[1][0] && pairs[0][0] <= pairs[1][1]) {
+    res += 1;
+    return;
+  }
+  if (pairs[0][1] <= pairs[1][1] && pairs[0][1] >= pairs[1][0]) {
+    res += 1;
+    return;
+  }
+  if (pairs[1][0] >= pairs[0][0] && pairs[1][0] <= pairs[0][1]) {
+    res += 1;
+    return;
+  }
+  if (pairs[1][1] <= pairs[0][1] && pairs[1][1] >= pairs[0][0]) {
     res += 1;
     return;
   }
